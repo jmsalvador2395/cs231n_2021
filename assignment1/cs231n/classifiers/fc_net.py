59c5d6e7afdb41574425d83cc8ec2fda13635b4b
john <jmsalvador2395@gmail.com>
2022-11-11 17:40:48 -0600

    still need to work on fc_nets in assignment 2

--- assignment1/cs231n/classifiers/fc_net.py
+++ assignment1/cs231n/classifiers/fc_net.py
@@ -7,116 +7,142 @@ from ..layer_utils import *
 
 
 class TwoLayerNet(object):
-    """
-    A two-layer fully-connected neural network with ReLU nonlinearity and
-    softmax loss that uses a modular layer design. We assume an input dimension
-    of D, a hidden dimension of H, and perform classification over C classes.
+	"""
+	A two-layer fully-connected neural network with ReLU nonlinearity and
+	softmax loss that uses a modular layer design. We assume an input dimension
+	of D, a hidden dimension of H, and perform classification over C classes.
 
-    The architecure should be affine - relu - affine - softmax.
+	The architecure should be affine - relu - affine - softmax.
 
-    Note that this class does not implement gradient descent; instead, it
-    will interact with a separate Solver object that is responsible for running
-    optimization.
+	Note that this class does not implement gradient descent; instead, it
+	will interact with a separate Solver object that is responsible for running
+	optimization.
 
-    The learnable parameters of the model are stored in the dictionary
-    self.params that maps parameter names to numpy arrays.
-    """
+	The learnable parameters of the model are stored in the dictionary
+	self.params that maps parameter names to numpy arrays.
+	"""
 
-    def __init__(
-        self,
-        input_dim=3 * 32 * 32,
-        hidden_dim=100,
-        num_classes=10,
-        weight_scale=1e-3,
-        reg=0.0,
-    ):
-        """
-        Initialize a new network.
+	def __init__(
+		self,
+		input_dim=3 * 32 * 32,
+		hidden_dim=100,
+		num_classes=10,
+		weight_scale=1e-3,
+		reg=0.0,
+	):
+		"""
+		Initialize a new network.
 
-        Inputs:
-        - input_dim: An integer giving the size of the input
-        - hidden_dim: An integer giving the size of the hidden layer
-        - num_classes: An integer giving the number of classes to classify
-        - weight_scale: Scalar giving the standard deviation for random
-          initialization of the weights.
-        - reg: Scalar giving L2 regularization strength.
-        """
-        self.params = {}
-        self.reg = reg
+		Inputs:
+		- input_dim: An integer giving the size of the input
+		- hidden_dim: An integer giving the size of the hidden layer
+		- num_classes: An integer giving the number of classes to classify
+		- weight_scale: Scalar giving the standard deviation for random
+		  initialization of the weights.
+		- reg: Scalar giving L2 regularization strength.
+		"""
+		self.params = {}
+		self.reg = reg
 
-        ############################################################################
-        # TODO: Initialize the weights and biases of the two-layer net. Weights    #
-        # should be initialized from a Gaussian centered at 0.0 with               #
-        # standard deviation equal to weight_scale, and biases should be           #
-        # initialized to zero. All weights and biases should be stored in the      #
-        # dictionary self.params, with first layer weights                         #
-        # and biases using the keys 'W1' and 'b1' and second layer                 #
-        # weights and biases using the keys 'W2' and 'b2'.                         #
-        ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+		############################################################################
+		# TODO: Initialize the weights and biases of the two-layer net. Weights    #
+		# should be initialized from a Gaussian centered at 0.0 with			   #
+		# standard deviation equal to weight_scale, and biases should be		   #
+		# initialized to zero. All weights and biases should be stored in the	   #
+		# dictionary self.params, with first layer weights						   #
+		# and biases using the keys 'W1' and 'b1' and second layer				   #
+		# weights and biases using the keys 'W2' and 'b2'.						   #
+		############################################################################
+		# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+		self.num_layers=2
+		dims=[input_dim] + [hidden_dim]*(self.num_layers-1) + [num_classes]
 
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-        ############################################################################
-        #                             END OF YOUR CODE                             #
-        ############################################################################
+		for i in range(1, self.num_layers+1):
+			self.params[f'W{i}']=np.random.normal(scale=weight_scale, size=(dims[i-1], dims[i]))
+			self.params[f'b{i}']=np.zeros(dims[i])
 
-    def loss(self, X, y=None):
-        """
-        Compute loss and gradient for a minibatch of data.
+		# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+		############################################################################
+		#							  END OF YOUR CODE							   #
+		############################################################################
 
-        Inputs:
-        - X: Array of input data of shape (N, d_1, ..., d_k)
-        - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+	def loss(self, X, y=None):
+		"""
+		Compute loss and gradient for a minibatch of data.
 
-        Returns:
-        If y is None, then run a test-time forward pass of the model and return:
-        - scores: Array of shape (N, C) giving classification scores, where
-          scores[i, c] is the classification score for X[i] and class c.
+		Inputs:
+		- X: Array of input data of shape (N, d_1, ..., d_k)
+		- y: Array of labels, of shape (N,). y[i] gives the label for X[i].
 
-        If y is not None, then run a training-time forward and backward pass and
-        return a tuple of:
-        - loss: Scalar value giving the loss
-        - grads: Dictionary with the same keys as self.params, mapping parameter
-          names to gradients of the loss with respect to those parameters.
-        """
-        scores = None
-        ############################################################################
-        # TODO: Implement the forward pass for the two-layer net, computing the    #
-        # class scores for X and storing them in the scores variable.              #
-        ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+		Returns:
+		If y is None, then run a test-time forward pass of the model and return:
+		- scores: Array of shape (N, C) giving classification scores, where
+		  scores[i, c] is the classification score for X[i] and class c.
 
-        pass
+		If y is not None, then run a training-time forward and backward pass and
+		return a tuple of:
+		- loss: Scalar value giving the loss
+		- grads: Dictionary with the same keys as self.params, mapping parameter
+		  names to gradients of the loss with respect to those parameters.
+		"""
+		scores = None
+		############################################################################
+		# TODO: Implement the forward pass for the two-layer net, computing the    #
+		# class scores for X and storing them in the scores variable.			   #
+		############################################################################
+		# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-        ############################################################################
-        #                             END OF YOUR CODE                             #
-        ############################################################################
+		scores=X
+		caches={}
+		for i in range(1, self.num_layers+1):
+			#affine forward
+			scores, cache=affine_forward(scores, self.params[f'W{i}'], self.params[f'b{i}'])
+			caches[f'affine{i}']=cache
 
-        # If y is None then we are in test mode so just return scores
-        if y is None:
-            return scores
+			#compute activation function if not on last layer
+			if i!=self.num_layers:
+				scores, caches[f'relu{i}']=relu_forward(scores)
 
-        loss, grads = 0, {}
-        ############################################################################
-        # TODO: Implement the backward pass for the two-layer net. Store the loss  #
-        # in the loss variable and gradients in the grads dictionary. Compute data #
-        # loss using softmax, and make sure that grads[k] holds the gradients for  #
-        # self.params[k]. Don't forget to add L2 regularization!                   #
-        #                                                                          #
-        # NOTE: To ensure that your implementation matches ours and you pass the   #
-        # automated tests, make sure that your L2 regularization includes a factor #
-        # of 0.5 to simplify the expression for the gradient.                      #
-        ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+		# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+		############################################################################
+		#							  END OF YOUR CODE							   #
+		############################################################################
 
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-        ############################################################################
-        #                             END OF YOUR CODE                             #
-        ############################################################################
+		# If y is None then we are in test mode so just return scores
+		if y is None:
+			return scores
 
-        return loss, grads
+		loss, grads = 0, {}
+		############################################################################
+		# TODO: Implement the backward pass for the two-layer net. Store the loss  #
+		# in the loss variable and gradients in the grads dictionary. Compute data #
+		# loss using softmax, and make sure that grads[k] holds the gradients for  #
+		# self.params[k]. Don't forget to add L2 regularization!				   #
+		#																		   #
+		# NOTE: To ensure that your implementation matches ours and you pass the   #
+		# automated tests, make sure that your L2 regularization includes a factor #
+		# of 0.5 to simplify the expression for the gradient.					   #
+		############################################################################
+		# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+		loss, dx=softmax_loss(scores, y)
+		loss+=.5*self.reg*np.sum(np.array([np.sum(self.params[f'W{i}']**2) for i in range(1, self.num_layers+1)]))
+
+		for i in range(self.num_layers, 0, -1):
+			#relu backwards
+			if i!=self.num_layers:
+				dx=relu_backward(dx, caches[f'relu{i}'])
+			
+			#affine backwards
+			dx, grads[f'W{i}'], grads[f'b{i}']=affine_backward(dx, caches[f'affine{i}'])
+			grads[f'W{i}']+=self.reg*self.params[f'W{i}']
+
+
+		# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+		############################################################################
+		#							  END OF YOUR CODE							   #
+		############################################################################
+
+		return loss, grads
